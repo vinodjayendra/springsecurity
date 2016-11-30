c6e20e3a6ec1f9b3c2447cd4401ec51034f74514
Mangala KJ <Mangala KJ>
2016-11-29 22:59:30 -0500

    First commit

---
+++ src/net/codejava/spring/MainController.java
@@ -0,0 +1,25 @@
+package net.codejava.spring;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/eod")
+public class MainController {
+	
+
+	
+	@GetMapping(path = "/simplepage", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public String visitHomeJson() {
+		return "vinod";
+	}
+	
+	
+	@RequestMapping(value="/adminjson", method = RequestMethod.GET)
+	public String visitAdminJson() {
+		return "vinod";
+	}
+}
